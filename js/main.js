3170067e1841483c72402a5a01ba04e2f3caa4cc
ballo <ballo.aleksej@gmail.com>
2022-12-03 14:24:55 +0300

    change main

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 function getRandomInt(from, to) {
-  if (from < 0 || to < 0) {
-    return NaN;
-  } else if (to < from) {
-    return NaN;
-  } else if (from === to) {
-    return from;
-  }
-  return from + Math.floor(Math.random() * (to - from + 1));
+  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
+  const upper = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-function checkString(str, maxLength) {
+function checkStringLength(str, maxLength) {
   return str.length <= maxLength;
 }
 
 getRandomInt(0, 10);
-checkString('1234567', 7);
+checkStringLength('1234567', 7);
 

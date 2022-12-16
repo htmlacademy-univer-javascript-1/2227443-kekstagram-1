a3bf3136f20efae8e12306417ef5b52efa9bffa2
ballo <ballo.aleksej@gmail.com>
2022-12-16 14:34:39 +0300

    delegate a lot of event-listeners to one event-listener

--- js/add-picture.js
+++ js/add-picture.js
@@ -4,21 +4,29 @@ import {showBigPicture} from './big-picture.js';
 const pictureTemplate = document.querySelector('#picture').content
   .querySelector('.picture');
 
+const pictureListElement = document.querySelector('.pictures');
 const pictureListFragment = document.createDocumentFragment();
 
-const createPictureElement = ({url, likes, comments, description}) => {
+const createPictureElement = ({id, url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.querySelector('.picture__likes').textContent = likes;
-
-  pictureElement.addEventListener('click', () => {
-    showBigPicture(url, likes, comments, description);
-  });
+  pictureElement.dataset.id = id;
   pictureListFragment.appendChild(pictureElement);
+};
+
+const onPictureClick = (evt) => {
+  const pictureElement = evt.target.closest('.picture');
+  if (pictureElement) {
+    const clickedPhoto = pictures.find(
+      ({id}) => Number(pictureElement. dataset.id) === id);
+    showBigPicture(clickedPhoto);
+  }
 };
 
 export const renderPictures = () => {
   pictures.forEach(createPictureElement);
-  document.querySelector('.pictures').appendChild(pictureListFragment);
+  pictureListElement.appendChild(pictureListFragment);
+  pictureListElement.addEventListener('click', onPictureClick);
 };
